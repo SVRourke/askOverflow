13ce68129e11f018cb9a7af9f6f077dc3051d597
SVR <samuel@rourke.tech>
2020-06-03 17:01:05 -0400

    placeholder App object

---
+++ lib/askoverflow/app.rb
@@ -0,0 +1,18 @@
+class App
+    def initialize
+        @cli = Cli.new
+        @scraper = Scrape.new
+    end
+
+    def run
+        # greet user
+        @cli.greet
+        # get search terms
+
+        # scrape term
+        # display results
+        # if result selected
+            # display specific result
+    end
+
+end
