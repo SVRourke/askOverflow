fbb3f7b4a7fd1560125c749633cbc8a91f2f9196
SVR <samuel@rourke.tech>
2020-06-10 16:51:56 -0400

    Finished cli tests

--- spec/03_cli_spec.rb
+++ spec/03_cli_spec.rb
@@ -1,56 +1,56 @@
 # spec/03_cli/spec.rb
-require 'rspec'
-require 'askoverflow'
+# require 'rspec'
+require 'spec_helper'
 
 RSpec.describe CLI do
-    before(:each) do
-        ui = CLI.new
-        test_hash = {
+    before(:all) do
+        @ui = CLI.new
+        @test_hash = {
             :question => "QUESTION",
             :sample => "SAMPLE",
             :tags => "TAGS",
             :ask_date => "DATE",
             :answer_count => "ANSWERS",
             :author => "AUTHOR",
-            :full_a => "FULLANSWER"
+            :full_a => "FULLANSWER",
             :full_q => "FULLQUESTION"
         }
-        Result.new_from_scrape(test_hash)
+        # # Result.new_from_scrape(test_hash)
     end
     describe "#greet" do
         it "greets the user" do
-            ui.greet
-            STDOUT.should_receive(:puts).with('AskOverflow')
+            expect {@ui.greet}.to output("askoverflow\n").to_stdout
         end
     end
     describe "#prompt_search" do
         it "asks user for search term" do
-            ui.prompt_search
-            STDOUT.should_receive(:puts).with('please enter a search or exit: ')
+            expect {@ui.prompt_search}.to output("please enter a search or exit: \n").to_stdout
         end
     end
     describe "#prompt_result" do
         it "asks the user for the id of their chosen result" do
-            ui.prompt_result
-            STDOUT.should_receive(:puts).with('enter the id of a result')
+            expect {@ui.prompt_result}.to output("enter the id of a result to read further, back to do a new search or exit to quit\n").to_stdout
         end
     end
     describe "#prompt_return" do
         it "asks if a user would like to return to the results or leave" do
-            ui.prompt_return
-            STDOUT.should_receive(:puts).with('back / exit?')
+            expect {@ui.prompt_return}.to output("\n\n\n+++enter to return to results+++\n\n\n\n").to_stdout
         end
     end
     describe "#display_results" do
         it "prints results to the terminal" do
-            ui.display_results
-            STDOUT.should_receive(:puts).with('1 ||QUESTION||SAMPLE||TAGS||DATE||ANSWERS||AUTHOR')
+            Result.new(@test_hash)
+            expect {@ui.display_results}.to output("++++++++++++++++++++++++++++++++++++++++++++++++++\n|ID:1| QUESTION...\n|ANSWERS answer(s) since: DATE\n|Tags:\tTAGS\n++++++++++++++++++++++++++++++++++++++++++++++++++\n\n").to_stdout
         end
     end
-    describe "#display_specific" do
-        it "prints the chosen result to terminal in high detail" do
-            ui.display_specific
-            STDOUT.should_receive(:puts).with('FULLQUESTION||FULLANSWER')
+    describe "#display_question" do
+        it "prints the full question text to the terminal" do
+            expect {@ui.display_question(Result.all.first)}.to output("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n++++++++++++++++++++++++++\n+++++++++QUESTION+++++++++\n++++++++++++++++++++++++++\nFULLQUESTION\n").to_stdout
+        end
+    end
+    describe "#display_answer" do
+        it "prints the full answer text to the terminal" do
+            expect {@ui.display_answer(Result.all.first)}.to output("\n\n\n\n\n\n\n\n\n\n\n\n++++++++++++++++++++++++++\n++++++++++ANSWER++++++++++\n++++++++++++++++++++++++++\nFULLANSWER\n").to_stdout
         end
     end
 end
